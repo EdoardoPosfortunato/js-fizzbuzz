745061d261fc27f90f2ce2543e728950ffc2506d
EdoardoPosfortunato <edoardo.posfortunato27@gmail.com>
2025-04-15 14:25:20 +0200

    feat:exercise

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// Raccolta Dati
+
+
+//Esecuzione Logica
+
+for (let i = 1; i <= 100; i++){
+    
+    if (i % 3 ===0 && i % 5 ===0){
+        console.log("Fizz Buzz")
+    } else if (i % 3 ===0){
+        console.log("buzz")
+    }else if (i % 5 ===0){
+            console.log("fizz")
+        }else{
+            console.log(i)
+        }
+    }
+
+
+//Output
