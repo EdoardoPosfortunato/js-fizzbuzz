fce6e892ae5de2acde665a8a64bcdc51da6dd1fa
EdoardoPosfortunato <edoardo.posfortunato27@gmail.com>
2025-04-15 15:26:13 +0200

    feat: tabelline

---
+++ js/script3.js
@@ -0,0 +1,10 @@
+for (let n = 1; n <= 10; n++) {
+
+    console.log(`Questa è a tabellina del ${n}`)
+
+    for (let i = 1; i <= 10; i++) {
+
+        console.log(`${n} x ${i} = ${n * i}`);
+
+        }
+}
